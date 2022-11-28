32e7311d6790f5a32cd4d1ede886c7695e190baf
Jerry Beans <nicklas@dundvig.net>
2022-11-28 08:43:20 +0100

    Database Connector Setup
    Ready for database connection! Rejoice!

--- SRC/src/DAL/db/DatabaseConnector.java
+++ SRC/src/DAL/db/DatabaseConnector.java
@@ -1,4 +1,24 @@
 package DAL.db;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+
 public class DatabaseConnector {
+    private SQLServerDataSource dataSource;
+
+    public MyDatabaseConnector()
+    {
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName("10.176.111.31");
+        dataSource.setDatabaseName("");
+        dataSource.setUser("");
+        dataSource.setPassword("");
+        dataSource.setTrustServerCertificate(true);
+    }
+
+    public Connection getConnection() throws SQLServerException {
+        return dataSource.getConnection();
+    }
 }
