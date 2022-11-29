897ce60ac8f6910f380cc0ff6233a87f52de0e62
ShadySloth <pedant01@easv365.dk>
2022-11-29 10:19:52 +0100

    even more GUI

--- SRC/src/GUI/Controller/MyTunesController.java
+++ SRC/src/GUI/Controller/MyTunesController.java
@@ -1,4 +1,76 @@
 package GUI.Controller;
 
-public class MyTunesController {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MyTunesController implements Initializable {
+
+    public ListView lstSong;
+    public ImageView imgSearch;
+    public ImageView imgUpArrow;
+    public ImageView imgDownArrow;
+    public ImageView imgLeftArrow;
+    public ImageView imgBack;
+    public ImageView imgPlay;
+    public ImageView imgNext;
+    public ImageView imgVolume;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+    public void handleNewPlaylist(ActionEvent actionEvent) {
+    }
+
+    public void handleEditPlaylist(ActionEvent actionEvent) {
+    }
+
+    public void handleDelete(ActionEvent actionEvent) {
+    }
+
+    public void handleSongUp(ActionEvent actionEvent) {
+    }
+
+    public void handleSongDown(ActionEvent actionEvent) {
+    }
+
+    public void handlePlaylistDeleteSong(ActionEvent actionEvent) {
+    }
+
+    public void handleAdd(ActionEvent actionEvent) {
+    }
+
+    public void handleNewSong(ActionEvent actionEvent) {
+    }
+
+    public void handleEditSong(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteSong(ActionEvent actionEvent) {
+    }
+
+    public void handleClose(ActionEvent actionEvent) {
+    }
+
+    public void handleSearch(ActionEvent actionEvent) {
+    }
+
+    public void handleBack(ActionEvent actionEvent) {
+    }
+
+    public void handlePlay(ActionEvent actionEvent) {
+    }
+
+    public void handleNext(ActionEvent actionEvent) {
+    }
+
+    public void handleVolume(MouseEvent mouseEvent) {
+    }
 }
--- SRC/src/Main.java
+++ SRC/src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("GUI/View/MyTunesView.fxml"));
+
+        primaryStage.setTitle("MyTunes");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world! :D");
+        launch(args);
     }
 }
